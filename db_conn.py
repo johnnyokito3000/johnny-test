59bb7c4dc7b0316517318adb479504179d5c144c
Johnny Okito <okito@okospartners.com>
2024-11-22 23:54:59 -0500

    created a .env file for specific configurations

--- db_conn.py
+++ db_conn.py
@@ -1,8 +1,14 @@
 import mysql.connector
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='123456',
-        database='my_company'
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
     )
+
